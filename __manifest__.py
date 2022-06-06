c748ea5b47f136aec6963f8b962dba7aba7a6227
levibarnes <lbarnesdev@gmail.com>
2022-06-06 18:56:05 +0000

    update manifest

--- __manifest__.py
+++ __manifest__.py
@@ -3,24 +3,23 @@
     'name': "project-agile",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        modify project to fit an agile project kanban""",
 
     'description': """
-        Long description of module's purpose
+        modify project to fit an agile project kanban
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Good Medicine Solutions",
+    'website': "https://www.barnesfam.us",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Library/Project',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','project'],
 
     # always loaded
     'data': [
