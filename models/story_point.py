1c7a5fe4642fa9d3bfdac104ea32ea05865ed64c
levibarnes <lbarnesdev@gmail.com>
2022-06-17 05:26:36 +0000

    update story point with _rec_name field

--- models/story_point.py
+++ models/story_point.py
@@ -3,5 +3,6 @@ from odoo import models, fields
 class ProjectAgileStoryPoint(models.Model):
     _name = 'project.agile.story_point'
     _description = 'Project Agile Story Point'
+    _rec_name = 'size'
     size = fields.Integer('size')
     description = fields.Html('description')
