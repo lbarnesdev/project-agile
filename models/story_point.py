b00879927b58291cdda6ee37954017a6cf99504d
levibarnes <lbarnesdev@gmail.com>
2022-06-17 05:29:39 +0000

    add task_ids to story point

--- models/story_point.py
+++ models/story_point.py
@@ -4,5 +4,7 @@ class ProjectAgileStoryPoint(models.Model):
     _name = 'project.agile.story_point'
     _description = 'Project Agile Story Point'
     _rec_name = 'size'
+    _order = 'size,description'
     size = fields.Integer('size')
     description = fields.Html('description')
+    task_ids = fields.One2many('project.task', 'storypoint_id', string='Task')
